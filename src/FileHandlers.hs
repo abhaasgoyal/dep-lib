6bcc58ee769333746c0ac8eb35a162d80a3f6c6e
abhaasgoyal <u7145384@anu.edu.au>
2021-06-08 02:06:20 +0530

    Add file handlers and data for tests

---
+++ src/FileHandlers.hs
@@ -0,0 +1,25 @@
+module FileHandlers
+  ( printOutput
+  , checkInput
+  , extractDepsFromFile
+  ) where
+
+import qualified Data.Set as S
+import           DepParser
+
+printOutput :: String -> [String] -> IO ()
+printOutput parentDep childDep = do
+  putStrLn $ parentDep ++ " depends on " ++ unwords childDep
+
+-- | Check whether the string "depends on" appears on correct location
+checkInput :: [[String]] -> Bool
+checkInput = all helpCheckDepend
+ where
+  helpCheckDepend :: [String] -> Bool
+  helpCheckDepend s | length s < 4                         = True
+                    | (s !! 1) ++ (s !! 2) == "depends on" = True
+                    | otherwise                            = False
+
+-- | Model dependencies read from the file
+extractDepsFromFile :: [[String]] -> [Dependencies]
+extractDepsFromFile = map (\s -> (head s, S.fromList $ drop 3 s))
